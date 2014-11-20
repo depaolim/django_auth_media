49908e195f83ed7b193fb77611ea9828e5e6d141
Marco De Paoli <depaolim@gmail.com>
2014-11-20 15:48:37 +0100

    xaccel in backends.py

--- auth_media/views.py
+++ auth_media/views.py
@@ -1,26 +1,22 @@
-import mimetypes
-import os
 import re
 
 from django.conf import settings
 from django.conf.urls import patterns, url
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponseNotFound
+from django.utils.module_loading import import_by_path
 
 from .models import do_check_auth
 
+BACKENDS = {}
 
-def do_serve(request, path, media_root=None, media_redirect=None):
-    if not media_root:
-        media_root = settings.MEDIA_ROOT
-    if not media_redirect:
-        media_redirect = settings.MEDIA_REDIRECT
-    fpath = os.path.join(media_root, path)
-    bname = os.path.basename(path)
-    content_type, encoding = mimetypes.guess_type(fpath)
-    response = HttpResponse(content_type=content_type)
-    response['Content-Disposition'] = "attachment; filename={}".format(bname)
-    response['X-Accel-Redirect'] = os.path.join(media_redirect, path)
-    return response
+
+for name, props in settings.MEDIA_SERVERS.items():
+    engine_path = props['ENGINE']
+    _do_serve = import_by_path(engine_path)
+    BACKENDS[name] = _do_serve
+
+
+do_serve = BACKENDS["xaccell"]
 
 
 def serve(
