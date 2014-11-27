39732f1b2ba9e75696d37e84de459b59e705f680
Marco De Paoli <depaolim@gmail.com>
2014-11-27 10:21:07 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+CLASSIFIERS = [
+    'Development Status :: 2 - Pre-Alpha',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+]
+
+setup(
+    author="Marco De Paoli",
+    author_email="depaolim@gmail.com",
+    name='django-auth-media',
+    version='0.0.1',
+    description='Django media files: check authorization and serve',
+    url='https://github.com/depaolim/django_auth_media',
+    license='BSD License',
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    install_requires=[
+        'setuptools',
+        'Django==1.6.7',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False
+)
